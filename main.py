cdc261d5b91c9eca8f1d9b053a970de598fa2d0f
fyfyfyfik <fyfyfyfik@gmail.com>
2021-06-04 02:28:25 +0300

    got it

---
+++ main.py
@@ -0,0 +1,47 @@
+# https://ru.wikibooks.org/wiki/SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model import keys
+import socket
+import json
+import os
+import config
+
+port = config.PORT
+
+s = socket.socket()
+s.bind((host, port))
+s.listen(2)
+file = os.getcwd() + '/keys.json'
+
+def main():
+    print(f'Server listen {port}')
+    while True:
+        conn, addr = s.accept()
+        if conn:
+
+            engine = create_engine('sqlite:///db.sqlite')
+            Session = sessionmaker(bind=engine)
+            session = Session()
+            Key = session.query(key).all()
+            arr_data = []
+            with open(file, 'w') as f:
+                for kk in Key:
+                    arr_data.append(
+                        {"Akey": kk.Akey,
+                        "Cname": kk.Cname,
+                        "Edate": kk.Edate,
+                        "Ktype": kk.Ktype,
+                         })
+                f.write(json.dumps({"data": arr_data}, indent=4))
+        f = open(file,'rb')
+        l = f.read(1024)
+        while (l):
+            conn.send(l)
+            l = f.read(1024)
+        f.close()
+        print(f'Successfully sending to {addr}')
+        conn.close()
+
+if __name__ == "__main__":
+    main()
