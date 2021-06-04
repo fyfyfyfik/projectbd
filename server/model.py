a5f11dce6bc5ac1e4dd873a34da33cde3b331d90
fyfyfyfik <fyfyfyfik@gmail.com>
2021-06-05 01:24:59 +0300

    got it

--- server/model.py
+++ server/model.py
@@ -4,53 +4,60 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 import json
+
 # https://docs.sqlalchemy.org/en/13/orm/extensions/declarative/basic_use.html
 DeclarativeBase = declarative_base()
 
-class Stories(DeclarativeBase):
-    __tablename__ = "stories"
+
+class keys(DeclarativeBase):
+    tablename = "keys"
 
     id = Column('id', Integer, primary_key=True)
-    hero = Column('hero', String)
-    story = Column('story', String)
-    end = Column('end', String)
+    Akey = Column('Akey', String)
+    Cname = Column('Cname', String)
+    Edate = Column('Edate', String)
+    Ktype = Column('Ktype', String)
 
-    def __init__(self, hero, story, end):
-        self.hero = hero
-        self.story = story
-        self.end = end
+    def init(self, Akey, Cname, Edate, Ktype):
+        self.Akey = Akey
+        self.Cname = Cname
+        self.Edate = Edate
+        self.Ktype = Ktype
 
-def add_stories(file_name="add_story.json"):
-    #Создаем объект Engine, который будет использоваться объектами ниже для связи с БД
+
+def add_keys(file_name="add_key.json"):
+    # Создаем объект Engine, который будет использоваться объектами ниже для связи с БД
     # engine = create_engine('postgresql://test:password@localhost:5432/project13')
     engine = create_engine('sqlite:///db.sqlite')
-    #Метод create_all создает таблицы в БД , определенные с помощью  DeclarativeBase
+    # Метод create_all создает таблицы в БД , определенные с помощью  DeclarativeBase
     DeclarativeBase.metadata.create_all(engine)
-    # Создаем фабрику для создания экземпляров Session. Для создания фабрики в аргументе 
+    # Создаем фабрику для создания экземпляров Session. Для создания фабрики в аргументе
     # bind передаем объект engine
     Session = sessionmaker(bind=engine)
     # Создаем объект сессии из вышесозданной фабрики Session
     session = Session()
 
     # функция для получения историй из файла, созданного администратором или модератором
-    def get_stories():
+    def get_keys():
         with open(file_name, 'r') as f:
             # Парсим джейсон
-            stories = json.loads(f.read())
+            keys = json.loads(f.read())
         # Возвращаем данные
-        return stories['hero'], stories['story'], stories['end']
+        return keys['Akey'], keys['Cname'], keys['Edate'], keys['Ktype']
+
     # Получаем истории из файла
-    stories = get_stories()
+    keys = get_keys()
     # Создаем новый объект из модели История
-    new_horror = Stories(stories[0], stories[1], stories[2])
+    new_key = keys(keys[0], keys[1], keys[2], keys[3])
     # Добавляем
-    session.add(new_horror)
+    session.add(new_key)
     # Сохраняем
     session.commit()
     # Закрываем
     session.close()
 
-    print('Новая история добавлена в базу данных.')
-    
-if __name__ == "__main__":
-    add_stories()
+    print('New keys added')
+
+
+if __name__ == "main":
+    add_keys()
