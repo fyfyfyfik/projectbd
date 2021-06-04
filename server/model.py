48e08aace8a2fdbb54d16e091745765abcb813db
Tanya <holleratyaboyout@gmail.com>
2021-06-05 01:09:22 +0300

    Changed db column names

--- server/model.py
+++ server/model.py
@@ -44,7 +44,7 @@ def add_keys(file_name="add_key.json"):
     # Получаем истории из файла
     keys = get_stories()
     # Создаем новый объект из модели История
-    new_key = Stories(stories[0], stories[1], stories[2])
+    new_key = keys(keys[0], keys[1], keys[2])
     # Добавляем
     session.add(new_key)
     # Сохраняем
